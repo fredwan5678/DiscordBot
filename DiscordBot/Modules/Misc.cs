6954cb741a398890519bbabc9d64c5f63d822c80
Kaden Dreger <kkdreger@gmail.com>
2019-07-24 14:07:26 -0600

    First Commit

---
+++ DiscordBot/Modules/Misc.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            await Context.Channel.SendMessageAsync(message);
+        }
+    }
+}
