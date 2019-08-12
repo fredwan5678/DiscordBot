7b5d120fb9317756e184786c19e7c4ce731a9a17
MaxiumMeda <43559318+MaxiumMeda@users.noreply.github.com>
2019-08-12 14:40:41 -0600

    Minor cleanup

--- DiscordBot/Modules/DataSaving/IDataSaver.cs
+++ DiscordBot/Modules/DataSaving/IDataSaver.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiscordBot.Modules.DataSaving
+﻿namespace DiscordBot.Modules.DataSaving
 {
     public interface IDataSaver
     {
